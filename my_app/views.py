2b648f193a3cc77cfbcbfc1f4fe544c48a497f26
Julia Gonik <jgonik@mit.edu>
2020-09-18 02:39:08 -0700

    Add cat API

--- my_app/views.py
+++ my_app/views.py
@@ -9,7 +9,7 @@ posts = [{"title": "This is my 1st post!", "description": "this is my first desc
 
 @app.route("/")
 def index():
-    return render_template("index0.html", name=name, facts=facts, posts=posts)
+    return render_template("index2.html", name=name, facts=facts, posts=posts)
 
 @app.route("/change_name")
 def change_name():
