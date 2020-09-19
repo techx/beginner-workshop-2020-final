85f618c49e096dcb5635b929080fbca02e9d8695
Zoë Marschner <zoe.marschner@gmail.com>
2020-09-18 22:42:06 -0400

    databases

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,6 @@
-import os
+import os, config
 from flask import Flask, session, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 #load main config
 app.config.from_pyfile('../config.py') 
-
+db = SQLAlchemy(app)
 
 import my_app.views
+import my_app.models
